f3f361a844367e712d2da966fbf320b9bc3aaf03
Justin Zhu <justinthezhu@gmail.com>
2018-09-03 14:52:12 -0400

    Updated first markdown section

---
+++ Scripts/Section1.R
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------#
+#------Stat 110: Introduction to Probability------------------------------------#
+#---------------Justin Zhu Section 1--------------------------------------------#
+#-Adapted from Joe Blitzstein and Jessica Hwang's Introduction to Probability---#
+#-And Kenneth Baclawski's Introduction to Probability with R--------------------#
+#-------------------------------------------------------------------------------#
+
+## Getting started with R
+# Press CTRL-Return to have each line executed in the console
+1 + 1
+
+# When you are finished with the R session, you can always quit
+q()
+
+#
